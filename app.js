f113895ef17f4411a81e7c0c42a7c878eb9285c4
Robert Marquardt <robert.marquardt@digital-motion.de>
2017-02-20 14:59:42 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,56 @@
+var express = require("express");
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var path = require("path");
+
+app.use(express.static(path.join(__dirname + "/")));
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+function Tile(size) {
+    this.color = [r: 0, g: 0, b: 0];
+    this.size = size;
+}
+
+function Tilemap() {
+    this.data = [];
+}
+
+Tilemap.prototype.generate = function(size, tileSize) {
+    // reset the old array for safety
+    this.data = [];
+    for(var i = 0; i < size * size; i++) {
+        this.data.push(new Tile(tileSize));
+    }
+}
+
+var board = new Tilemap();
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+
+
+    socket.on("startGame", function(data) {
+        console.log("Creating new field...");
+
+        board.generate(data.fieldSize, data.tileSize);
+
+        io.emit("gameSetupComple", board);
+    });
+
+    socket.on("paintTile", function(data) {
+
+    });
+
+});
+
+http.listen(3000, function(){
+    console.log('listening on *:3000');
+});
